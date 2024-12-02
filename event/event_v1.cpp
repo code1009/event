3829f055f0a692a36c59dc57115053f7e9a6ee37
code1009 <code1009@naver.com>
2024-12-02 19:21:35 +0900

    1

--- event/event_v1.cpp
+++ event/event_v1.cpp
@@ -21,8 +21,8 @@ namespace v1
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-template<typename...Tevent_args>
-using event_handler = std::function<void(Tevent_args...)>;
+template<typename...Tevent_param>
+using event_handler = std::function<void(Tevent_param...)>;
 
 
 
@@ -42,22 +42,22 @@ public:
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-template<typename...Tevent_args>
-class event_args_listener : public event_listener
+template<typename...Tevent_param>
+class event_param_listener : public event_listener
 {
 public:
-	event_handler<Tevent_args...> _handler;
+	event_handler<Tevent_param...> _handler;
 
 public:
-	event_args_listener(event_handler<Tevent_args...> handler) :
+	event_param_listener(event_handler<Tevent_param...> handler) :
 		_handler(handler) 
 	{
 	}
 
 public:
-	void invoke(Tevent_args... args)
+	void invoke(Tevent_param... param)
 	{
-		_handler(args...);
+		_handler(param...);
 	}
 };
 
@@ -74,11 +74,11 @@ public:
 	std::unordered_map<Tevent_target, std::shared_ptr<event_listener>> _listeners;
 
 public:
-	template<typename...Tevent_args>
-	void register_handler(event_handler<Tevent_args...> handler, Tevent_target target = 0)
+	template<typename...Tevent_param>
+	void register_handler(event_handler<Tevent_param...> handler, Tevent_target target = 0)
 	{
 		register_listener(
-			std::make_shared<event_args_listener<Tevent_args...>>(std::move(handler)),
+			std::make_shared<event_param_listener<Tevent_param...>>(std::move(handler)),
 			target
 		);
 	}
@@ -94,29 +94,29 @@ public:
 	}
 
 public:
-	template<typename...Tevent_args>
-	void broadcast(Tevent_args... args)
+	template<typename...Tevent_param>
+	void broadcast(Tevent_param... param)
 	{
 		for (auto& listener : _listeners)
 		{
-			auto l = std::dynamic_pointer_cast<event_args_listener<Tevent_args...>>(listener.second);
+			auto l = std::dynamic_pointer_cast<event_param_listener<Tevent_param...>>(listener.second);
 			if (l)
 			{
-				l->invoke(args...);
+				l->invoke(param...);
 			}
 		}
 	}
 
-	template<typename...Tevent_args>
-	void unicast(Tevent_target target, Tevent_args... args)
+	template<typename...Tevent_param>
+	void unicast(Tevent_target target, Tevent_param... param)
 	{
 		auto found = _listeners.find(target);
 		if (found != _listeners.end())
 		{
-			auto l = std::dynamic_pointer_cast<event_args_listener<Tevent_args...>>((*found).second);
+			auto l = std::dynamic_pointer_cast<event_param_listener<Tevent_param...>>((*found).second);
 			if (l)
 			{
-				l->invoke(args...);
+				l->invoke(param...);
 			}
 		}
 	}
@@ -138,20 +138,20 @@ public:
 	> _targeted_event_dispatchers;
 
 public:
-	template<typename...Tevent_args>
-	void register_event(Tevent_type name, Tevent_target target, event_handler<Tevent_args...> handler)
+	template<typename...Tevent_param>
+	void register_event(Tevent_type name, Tevent_target target, event_handler<Tevent_param...> handler)
 	{
 		auto found = _targeted_event_dispatchers.find(name);
 		if (found != _targeted_event_dispatchers.end())
 		{
 			auto dispatcher = (*found).second;
-			dispatcher->register_handler<Tevent_args...>(handler, target);
+			dispatcher->register_handler<Tevent_param...>(handler, target);
 		}
 		else
 		{
 			auto dispatcher = std::make_shared<targeted_event_dispatcher<Tevent_target>>();
 			_targeted_event_dispatchers[name] = dispatcher;
-			dispatcher->register_handler<Tevent_args...>(handler, target);
+			dispatcher->register_handler<Tevent_param...>(handler, target);
 		}
 	}
 
@@ -165,25 +165,25 @@ public:
 	}
 
 public:
-	template<typename...Tevent_args>
-	void dispatch(Tevent_type name, Tevent_args... args)
+	template<typename...Tevent_param>
+	void dispatch(Tevent_type name, Tevent_param... param)
 	{
 		auto found = _targeted_event_dispatchers.find(name);
 		if (found != _targeted_event_dispatchers.end())
 		{
 			auto dispatcher = (*found).second;
-			dispatcher->broadcast<Tevent_args...>(args...);
+			dispatcher->broadcast<Tevent_param...>(param...);
 		}
 	}
 
-	template<typename...Tevent_args>
-	void dispatch(Tevent_type name, Tevent_target target, Tevent_args... args)
+	template<typename...Tevent_param>
+	void dispatch(Tevent_type name, Tevent_target target, Tevent_param... param)
 	{
 		auto found = _targeted_event_dispatchers.find(name);
 		if (found != _targeted_event_dispatchers.end())
 		{
 			auto dispatcher = (*found).second;
-			dispatcher->unicast<Tevent_args...>(target, args...);
+			dispatcher->unicast<Tevent_param...>(target, param...);
 		}
 	}
 };
@@ -203,24 +203,24 @@ using my_event_dispatcher = typed_event_dispatcher<std::string, my_event_observe
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-class my_event_type_data1
+class my_event_t1_param
 {
 public:
 	int _a;
 
 public:
-	explicit my_event_type_data1(int a) : _a{ a }
+	explicit my_event_t1_param(int a) : _a{ a }
 	{
 	}
 };
 
-class my_event_type_data2
+class my_event_t2_param
 {
 public:
 	std::string _a;
 
 public:
-	explicit my_event_type_data2(std::string a) : _a{ a }
+	explicit my_event_t2_param(std::string a) : _a{ a }
 	{
 	}
 };
@@ -248,10 +248,10 @@ public:
 public:
 	void register_event_handler(my_event_dispatcher* dispatcher)
 	{
-		dispatcher->register_event<const my_event_type_data1&>(
+		dispatcher->register_event<const my_event_t1_param&>(
 			std::string{"t1"},
 			this,
-			[this](const my_event_type_data1& data)
+			[this](const my_event_t1_param& data)
 			{
 				this->on_event_t1_ignore(
 					data
@@ -259,7 +259,7 @@ public:
 			}
 		);
 
-		dispatcher->register_event<const my_event_type_data1&>(
+		dispatcher->register_event<const my_event_t1_param&>(
 			"t1",
 			this,
 			[this](auto data)
@@ -270,7 +270,7 @@ public:
 			}
 		);
 
-		dispatcher->register_event<const my_event_type_data2&>(
+		dispatcher->register_event<const my_event_t2_param&>(
 			"t2",
 			this,
 			[this](auto data)
@@ -283,17 +283,17 @@ public:
 	}
 
 public:
-	void on_event_t1_ignore(const my_event_type_data1& data)
+	void on_event_t1_ignore(const my_event_t1_param& data)
 	{
 		std::cout << _name << "-on_event_t1_ignore: " << data._a << std::endl;
 	}
 
-	void on_event_t1(const my_event_type_data1& data)
+	void on_event_t1(const my_event_t1_param& data)
 	{
 		std::cout << _name << "-on_event_t1: " << data._a << std::endl;
 	}
 
-	void on_event_t2(const my_event_type_data2& data)
+	void on_event_t2(const my_event_t2_param& data)
 	{
 		std::cout << _name << "-on_event_t2: " << data._a << std::endl;
 	}
@@ -321,10 +321,10 @@ void test_event_v1()
 	observer1.register_event_handler(&dispatcher);
 	observer2.register_event_handler(&dispatcher);
 
-	v1::my_event_type_data1 a{ 1 };
-	v1::my_event_type_data2 b{ "hello" };
+	v1::my_event_t1_param a{ 1 };
+	v1::my_event_t2_param b{ "hello" };
 
-	dispatcher.dispatch<const v1::my_event_type_data1&>("t1", a);
-	dispatcher.dispatch<const v1::my_event_type_data2&>("t2", b);
-	dispatcher.dispatch<const v1::my_event_type_data1&>("t1", &observer2, a);
+	dispatcher.dispatch<const v1::my_event_t1_param&>("t1", a);
+	dispatcher.dispatch<const v1::my_event_t2_param&>("t2", b);
+	dispatcher.dispatch<const v1::my_event_t1_param&>("t1", &observer2, a);
 }
